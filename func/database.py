1bc0d93af2c3087ed0fe8ccfabe4f4d306200cbb
hoshikuu <145494860+Hoshikuu@users.noreply.github.com>
2025-10-03 12:47:03 +0200

    Updated: Database with dynamic select and insert

--- func/database.py
+++ func/database.py
@@ -38,12 +38,19 @@ def create_global_db():
     conn.close()
     logger.info("Base de datos global creada")
 
-def check_file():
-    """Comprueba que la base de datos este creada
+def check_file(id: str = None):
+    """Comprobar si existe la base de datos global y la de configuración
+
+    Args:
+        id (str, optional): El id de la base de datos de configuración. Defaults to None.
     """
+    if id != None and not isfile(f"database/{id}.db"):
+        logger.warning(f"No se ha encontrado la base de datos de configuración para el ID {id}, creando una nueva...")
+        create_db(id)
+    
     if isfile("database/global.db"):
         return
-    
+
     if not isdir("database"):
         logger.warning("Creando nueva carpeta de Database")
         mkdir("database")
@@ -127,5 +134,138 @@ def update_global_db(id: str, token: str):
         return True
     except Exception as e:
         logger.error(f"Error inesperado al actualizar el token en la base de datos global.db: {e}")
+        return False
+    
+def create_db(id: str):
+    """Crea la base de datos de configuración si no existe
+
+    Args:
+        id (str): ID del token
+    """
+    
+    logger.warning(f"Creando una nueva Base de Datos de Configuración para el ID {id}")
+    conn = connect(f"database/{id}.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE "config" (
+        "id"	INTEGER NOT NULL UNIQUE,
+        "setup"	INTEGER NOT NULL DEFAULT 0,
+        "prefix"	TEXT NOT NULL DEFAULT 'hs$',
+        "log"	TEXT,
+        PRIMARY KEY("id")
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE "op" (
+        "id"	TEXT NOT NULL UNIQUE,
+        "name"	TEXT,
+        PRIMARY KEY("id")
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE "ticket" (
+        "id"	INTEGER NOT NULL UNIQUE,
+        "general"	TEXT NOT NULL,
+        "category"	TEXT NOT NULL,
+        "member"	TEXT NOT NULL,
+        "message"	TEXT NOT NULL DEFAULT 'Welcome',
+        PRIMARY KEY("id")
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE "ticket_op" (
+        "id"	TEXT NOT NULL UNIQUE,
+        "name"	TEXT,
+        PRIMARY KEY("id")
+    );  
+    """)
+
+    cursor.execute("""
+    CREATE TABLE "ticket_panels" (
+        "id"	TEXT NOT NULL UNIQUE,
+        "count"	TEXT NOT NULL,
+        "title"	TEXT NOT NULL,
+        "description"	TEXT NOT NULL,
+        "category"	TEXT NOT NULL,
+        "name"	TEXT NOT NULL,
+        PRIMARY KEY("id")
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE "data" (
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        date TEXT,
+        messages INTEGER DEFAULT 0,
+        voicechat FLOAT DEFAULT 0
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+    logger.info(f"Base de datos de configuración creada para {id}")
+
+
+def select_db(id: str, atribute: str, table: str, what: str, condition: str):    
+    """Selecciona datos de la base de datos
+
+    Args:
+        id (str): Id de la base de datos
+        atribute (str): El campo al que seleccionar, * para todos
+        table (str): La tabla de la base de datos
+        what (str): Que campo se va a comparar
+        condition (str): La condición a comparar
+
+    Returns:
+        tuple: Devuelve una tupla con los datos seleccionados, None si no se encuentra
+    """
+
+    check_file(id)
+    try:
+        conn = connect(f"database/{id}.db")
+        cursor = conn.cursor()
+        cursor.execute(
+        f"""
+        SELECT {atribute} FROM {table} WHERE {what} = {condition}
+        """)
+        data = cursor.fetchone()
+        conn.close()
+        return data
+    except Exception as e:
+        logger.error(f"Error inesperado al seleccionar datos de la base de datos {id}.db: {e}")
+        return None
+    
+def insert_db(id: str, table: str, values: str, data: tuple):
+    """Inserta datos en la base de datos
+
+    Args:
+        id (str): Id de la base de datos
+        table (str): La tabla de la base de datos
+        values (str): Los campos a insertar separados por comas
+        data (tuple): Una tupla con los datos a insertar
+
+    Returns:
+        bool: Devuelve True si se ha insertado correctamente, False si ha habido un error
+    """
+
+    check_file(id)
+    try:
+        conn = connect(f"database/{id}.db")
+        cursor = conn.cursor()
+        cursor.execute(
+        f"""
+        INSERT INTO {table} ({values}) VALUES ({','.join(['?' for _ in values.split(',')])})
+        """, data)
+        conn.commit()
+        conn.close()
+        logger.info(f"Datos insertados correctamente en la base de datos {id}.db")
+        return True
+    except Exception as e:
+        logger.error(f"Error inesperado al insertar datos en la base de datos {id}.db: {e}")
         return False
     
